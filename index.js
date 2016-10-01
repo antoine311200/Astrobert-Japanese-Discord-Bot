c6efe470f192697599173f0d88c72ac047f4021c
Antoine Debouchage <NattackDev@users.noreply.github.com>
2016-10-01 19:01:57 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -4,36 +4,98 @@ const client = new Discord.Client();
 const request = require("request");
 const jquery = require("cheerio");
 const http = require("http");
+const cool = require('cool-ascii-faces');
+const express = require('express');
+
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.get('/', function (request, response) {
+  response.render('pages/index')
+});
+app.get('/cool', function (request, response) {
+  response.send(cool());
+});
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
+});
 
 const BreakException = {};
 
-
 client.on("debug", msg => { return console.log("[debug]", msg); });
 client.on("unk", 	 msg => { return console.log("[unknown]", msg); });
-client.on('ready', () => {
-  console.log('[debug] Bot ready!');
-});
+client.on('ready', () =>  { return console.log('[debug] Bot ready!'); });
 client.on('message', message => {
-	
 	var msg = message.content.toLowerCase();
-	for (var cmd in commands) if (msg.indexOf(cmd) > -1) commands[cmd].callback(message);
-	
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
+	for (var cmd in commands) {
+		if (msg.indexOf(cmd) > -1 && message.author.id !== "185146510210629632") {
+			commands[cmd].callback(message);
+			break;
+		}
+	}
+	if(message.author.id === "177419389447045120" || message.author.id === "153561453440401409" || message.author.id === "185293714040684544") 
+		for (var cmd in admin_commands) if (msg.indexOf(cmd) > -1) admin_commands[cmd].callback(message);
 });
 client.login('MTg1MTQ2NTEwMjEwNjI5NjMy.CsuwKg.CoQlr8Nc03f6fBM3YEMJfNUE-UI').then("Bot connected !").catch(console.log);
 
 const commands = {};
+const admin_commands = {};
 
-commands["!help"] = {
-	token: "!aide",
-	description: "Liste des commandes",
-	
+admin_commands["!admin"] = {
+	token: "!admin",
+	description: "Liste des commandes administrateurs",
 	callback: message => {
 		let com = "";
-		for (let cmd in commands)  
-			com += "  • " + commands[cmd].token + " : " + commands[cmd].description + "\n";
+		for (let cmd in admin_commands)  
+			com += "  • " + admin_commands[cmd].token + " : " + admin_commands[cmd].description + "\n";
+		message.channel.sendMessage("```Commandes :\n"+com+"```");
+	}
+}
+
+admin_commands["!kick"] = {
+	token: "!kick <username>",
+	description: "Kick un membre",
+	
+	callback: message => {
+		let tok = "!kick";
+		let userID = (message.content.indexOf("<@") > 1) ? message.content.substring(tok.length).match("<@(.*)>")[1].replace("!", "") : message.content.substring(tok.length).trim();
+		console.log(userID);
+		client.fetchUser(userID).then(user => user.sendMessage(`
+Bonjour, nous avons décider de retirer les personnes non actives et étant hors-sujet du serveur "Apprentissage du Japonais" pour avoir une meilleure visibilité et une meilleure organisation des membres présents.\n
+Si jamais vous ne vous sentez pas concerné car nous avons fais une erreur et vous êtes actif/active ou voulez reprendre le japonais sérieusement et participer à la vie du serveur en partageant votre expérience, vos ressentis...\n
+Vous pouvez cliquer sur ce lien pour revenir parmis nous : https://discordapp.com/invite/011REhyDAdjaUI3Pc
+Passez une bonne journée ! :D
+
+(Ceci est un message d'Astrobert, bot discord spécialisé dans l'apprentissage du Japonais :p )
+		`).then("Message sent !").catch(console.log));
+		
+		message.guild.fetchMember(userID).then(user => user.kick().then(user => message.channel.sendMessage(`L'utilisateur ${user.nickname} a été correctement exclu du serveur`).catch(console.log))).catch(console.log);
+	}
+}
+
+admin_commands["!prune"] = {
+	token: "!prune <days>",
+	description: "Purge les membres inactifs et envoie un message de réinvitation",
+	
+	callback: message => {
+		const tok = "!prune";
+		const days = message.content.substring(tok.length);
+		message.guild.pruneMembers(parseInt(days))
+		.then(pruned => message.channel.sendMessage(`I just pruned ${pruned} people!`))
+  	.catch(console.error);
+	}
+}
+
+
+commands["!help"] = {
+	token: "!help",
+	description: "Liste des commandes",
+	callback: message => {
+		let com = "";
+		console.log(message.author, message.timestamp);
+		for (let cmd in commands)	com += "  • " + commands[cmd].token + " : " + commands[cmd].description + "\n";
 		message.channel.sendMessage("```Commandes :\n"+com+"```");
 	}
 }
@@ -41,7 +103,6 @@ commands["!help"] = {
 commands["!kanji"] = {
 	token: "!kanji <kanji>",
 	description: "Description d'un ou plusieurs kanjis",
-	
 	callback: message => {
 		const tok = "!kanji";
 		const kanji = message.content.substring(tok.length);
@@ -58,10 +119,10 @@ commands["!kanji"] = {
 						$(kanjis).each((index, div) => {
 							JQuerySend($, div, data => {
 								BotSendMessage(message, [
-									"__Kanji__ : "+data.kanji,
+									"```Kanji : "+data.kanji,
 									"  • on : " + data.on,
 									"  • kun : " + data.kun,
-									"  • Signification : *" + data.trans +"*"
+									"  • Signification : " + data.trans +"```"
 								])
 							});
 						});
@@ -94,13 +155,11 @@ commands["!word"] = {
 					try {
 						JsonSend(json, data => {
 							BotSendMessage(message, [
-								"__Word__ : ",
-								"  • word : " + data.kanji,
+								"```Word : " + data.kanji,
 								"  • reading : " + data.reading,
-								"  • signification : *" + data.senses +"*"
+								"  • signification : " + data.senses +"```"
 							]);
 						});
-						
 					}
 					catch(e) {
 						if(e !== BreakException) throw e;
@@ -128,8 +187,12 @@ setInterval(function () {
 	});
 }, interval);
 
-
-
+const getID = (id, users) => {
+	for(let user in users) 
+			if(user.id === id) return user;
+	
+	return false;
+}
 
 const BotSendMessage = (message, array) => {
 	var msg = "";
